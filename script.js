d4fa1c7257a777b2bf9ec786b5acc7d97035fec4
Valen <valen2000ar@gmail.com>
2025-06-08 12:37:10 -0300

    Le di funcionalidad al carrito Le di funcionalidad al menú Agruegué la sección de "Quienes somos" Mejoré el el diseño tsnto para pc como para celular Agregué nuevas imágenes y modifiqué algunas existentes

---
+++ script.js
@@ -0,0 +1,61 @@
+//Carrito
+let cart=[]; 
+
+function addToCart(nombre,precio){
+    let producto= cart.find(item=> item.nombre === nombre);
+    if(producto){
+        producto.cantidad++;
+    }else{
+        cart.push({nombre: nombre, precio: precio, cantidad: 1});
+    }
+    updateCart();
+}
+
+const savedCart = localStorage.getItem("cart");
+if (savedCart) {
+    cart = JSON.parse(savedCart);
+    updateCart();
+}
+
+function updateCart(){
+    let cartItems= document.getElementById("cart-items");
+    let cartCount= document.getElementById("cart-count");
+    let cartTotal= document.getElementById("cart-total");
+
+    cartItems.innerHTML = "";
+    let total = 0;
+    let count = 0;
+
+    cart.forEach(item => {
+        let subtotal= item.precio * item.cantidad;
+        total += subtotal;
+        count += item.cantidad;
+
+        let row= `<tr>
+                    <td>${item.nombre}</td>
+                    <td>${item.cantidad}</td>
+                    <td>${item.precio}</td>
+                    <td>${subtotal}</td>
+                    <td>
+                        <button class="btn btn-danger btn-sm" onclick="removeFromCart('${item.nombre}')">-</button>
+                        <button class="btn btn-success btn-sm" onclick="addToCart('${item.nombre}', ${item.precio})">+</button>
+                    </td>
+                </tr>`;
+        cartItems.innerHTML += row;
+    });
+    cartCount.textContent = count;
+    cartTotal.textContent = total;
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+function removeFromCart(nombre){
+    let producto = cart.find(item => item.nombre === nombre);
+    if (producto) {
+        producto.cantidad--;
+        if (producto.cantidad <= 0) {
+            cart = cart.filter(item => item.nombre !== nombre);
+        }
+    }
+    updateCart();
+}
